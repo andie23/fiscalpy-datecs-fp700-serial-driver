00170b98546fa0a79fe567ace75d33ed05703d77
Andrew Mfune <andiemfune@gmail.com>
2025-01-02 15:49:24 +0200

    improved text prompts

--- odoo_service/prompt.py
+++ odoo_service/prompt.py
@@ -5,17 +5,24 @@ import os
 import version
 import config
 
+def pause_screen():
+    os.system("pause")
+
+def clear_screen():
+    os.system("cls")
+
 def print_error(message):
+    clear_screen()
     print("********************ERROR********************")
     print("")
     print(message)
     print("")
-    print("*********************ERROR********************")
 
 def print_title(title):
+    clear_screen()
     print("")
-    print(f"================={title}=================")
-    print("")
+    print(f"{title}".upper())
+    print("--------------------------------------------")
 
 def prompt_free_text(message):
     '''
@@ -29,13 +36,14 @@ def prompt_selection(message, options):
     Input Prompt for selections of predefined lists
     '''
     count = 1
+    print_title(message)
     # Render the list of items to selecrt
     for option in options:
         print(f"{count}) {option['title']}")
         count+=1
     try:
         # Ask the user to enter the correct value from the list
-        index = int(prompt_free_text(message))-1
+        index = int(prompt_free_text("Enter option#"))-1
         return options[index]
     except Exception:
         print_error("Invalid Selection")
@@ -45,10 +53,11 @@ def reprint_order():
     print_title("Reprint Receipt")
     order_number = prompt_free_text("Enter Order#")
     if util.is_receipt_archived(order_number):
-        print(f"Reprinting {order_number}")
+        print_title(f"Reprinting {order_number}")
         util.print_archived(order_number)
     else:
         print_error(f"Order {order_number} not found")
+    pause_screen()
 
 def debugger():    
     print_title("View Receipt")
@@ -83,12 +92,13 @@ def debugger():
             dir_options
         )
         selection["action"]()
-    
-    selection = prompt_selection("Select option", [
+
+    selection = prompt_selection("PDF Reader options", [
         { "title": "Read PDF receipt text", "action": read_receipt_text },        
         { "title": "Read PDF receipt metadata", "action": read_receipt_dictionary },
     ])
     selection["action"]()
+    pause_screen()
 
 def print_pdf(pdf_list_limit=5):
     def dir_to_option(file):
@@ -98,7 +108,8 @@ def print_pdf(pdf_list_limit=5):
         }
 
     def print_manually():
-        filename = prompt_free_text("Please specify Filename")
+        clear_screen()
+        filename = prompt_free_text("Please specify Filename to print")
         util.print_from_pdf(os.path.join(parent_dir, filename))
 
     try:
@@ -115,12 +126,15 @@ def print_pdf(pdf_list_limit=5):
         }
         dir_options = [dir_to_option(file) for file in files]
         selection = prompt_selection(
-            f"Enter option#", 
+            f"Which PDF document do you want to print?", 
             [ *dir_options, list_all_files, manual_option ]
         )
+        clear_screen()
         selection["action"]()
+        pause_screen()
     except Exception as error:
         log.error(error)
+        pause_screen()
 
 def test_receipt():
     print_title("Connection Test")
@@ -142,18 +156,21 @@ def test_receipt():
                 { "title": "No", "action": lambda: print("") }
             ])
             another_option["action"]()
+            pause_screen()
 
         retry_selection = prompt_selection("Do you want to retry test?", [
             { "title": "Yes", "action": test_receipt }, 
             { "title": "No", "action": try_another_option }
         ])
         retry_selection["action"]()
+        pause_screen()
 
     test_success_selection = prompt_selection("Did you hear any Sound from the printer?", [
         { "title": "Yes", "action": lambda: print("Printer ok") },
         { "title": "No", "action": no_action }
     ])
     test_success_selection["action"]()
+    pause_screen()
 
 def receipt_settings():
     print_title("Receipt Settings")
@@ -180,6 +197,7 @@ def receipt_settings():
             util.update_payment_method(code_selection["value"], updated_list)
         else:
             print_error("Invalid selection")
+            pause_screen()
 
     def add_payment_method():
         print_title("Add new payment method")
@@ -197,6 +215,7 @@ def receipt_settings():
             util.update_payment_method(code_selection["value"], p_types)
         else:
             print_error("Invalid payment method")
+            pause_screen()
 
     def show_payment_methods():
         print_title("Supported payment methods")
@@ -207,6 +226,7 @@ def receipt_settings():
             for val in method["values"]:
                 print("")
                 print(f"==> {val}")
+        pause_screen()
         
     def set_error_receipts():
         print_title("Print Errors")
@@ -224,9 +244,11 @@ def receipt_settings():
             if os.path.exists(target_dir):
                 config.update(config.K_DOWNLOAD_FOLDER, val)
             else:
-                print_error("Invalid/Folder exist")                
+                print_error("Invalid/Folder exist") 
+                pause_screen()               
         except:
             print_error("Error occured validating path provided")
+            pause_screen()
 
     def set_date_validation():
         print_title("Date Validation")
@@ -269,11 +291,10 @@ def receipt_settings():
             return
     except:
         print("Invalid Option")
+        pause_screen()
     receipt_settings()
  
 def printer_settings():
-    print_title("Printer Settings")
-
     def set_baudrate():
         print_title("Baudrate")
         selection = prompt_selection("Select Baudrate", [
@@ -289,6 +310,7 @@ def printer_settings():
             util.run_printer_sdk(["--baudrate", selection["title"]])
         else:
             print("Invalid Selection")
+            pause_screen()
 
     def set_port():
         print_title("Port")
@@ -310,6 +332,7 @@ def printer_settings():
             util.run_printer_sdk(["--port", selection["title"]])
         else:
             print("Invalid option entered")
+            pause_screen()
 
     def set_copies():
         print_title("Receipt Copies")
@@ -329,7 +352,7 @@ def printer_settings():
         password = prompt_free_text("Please enter Operator Password")
         util.run_printer_sdk(["--operatorPassword", password])
 
-    selection = prompt_selection("Select setting#", [
+    selection = prompt_selection("Printer setting options", [
         { "title": "Baudrate", "action": set_baudrate },
         { "title": "Port", "action": set_port },
         { "title": "Print Copies", "action": set_copies },
@@ -344,11 +367,12 @@ def printer_settings():
             return
     except:
         print_error("Invalid Option")
+        pause_screen()
     printer_settings()
 
 def main():
     print_title("Main Menu")
-    selection = prompt_selection("Please enter option#", [
+    selection = prompt_selection("MRA Printer Utilities", [
         { "title": "Print Archived Receipt by Order#", "action": reprint_order },
         { "title": "Print a PDF Receipt File", "action": print_pdf },
         { "title": "Verify printer is connected", "action": test_receipt },
@@ -360,6 +384,7 @@ def main():
         selection["action"]()
     except:
         print_error("Invalid Option")
+        pause_screen()
     main()
 
 if __name__ == "__main__":
